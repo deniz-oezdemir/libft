2dc9dfa08ecce10dd340fab413ce4a89fac9fec8
Deniz Ozdemir <denizozd@c4b4c1.42berlin.de>
2023-11-20 13:20:42 +0100

    debugged

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: denizozd <denizozd@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:18:58 by denizozd          #+#    #+#             */
-/*   Updated: 2023/11/17 18:33:34 by denizozd         ###   ########.fr       */
+/*   Updated: 2023/11/20 13:18:37 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new = NULL;
 	while (lst)
 	{
-		tmp = ft_lstnew((*f)(lst->content));
+		tmp = ft_lstnew(f(lst->content));
 		if (!tmp)
 		{
 			ft_lstclear(&new, del);
